8a878aa106412675eaa1f577fc45fa9cb9c40abb
At-sushi <dkahw210@kyoto.zaq.ne.jp>
2025-03-02 18:22:30 +0900

    コードの可読性向上とスタイルの統一
    `2chDB.cpp`のインデントを整え、可読性を向上させました。
    `using namespace std;`を削除し、`std::`プレフィックスを追加して名前空間の衝突を回避しました。
    `main`関数内のコードを整理し、無限ループを追加しましたが、実際の処理は行われていません。
    `init`関数や`queryFromReadCGI`関数内のインデントも整えられ、全体的にコードスタイルが統一され、エラーメッセージの出力が改善されました。

--- 2chDB/2chDB.cpp
+++ 2chDB/2chDB.cpp
@@ -5,8 +5,8 @@
 
 extern "C" {
 #include "readcgi/read.h"
-	extern int rawmode, isbusytime;
-	extern void atexitfunc();
+    extern int rawmode, isbusytime;
+    extern void atexitfunc();
 }
 
 void init();
@@ -14,43 +14,43 @@ void testWrite(const char* bbs, const char* key, const char* source);
 const char* queryFromReadCGI(const char* bbs, const char* key);	
 
 inline static constexpr std::string create_fname(const char* bbs, const char* key) {
-	return std::format("{}/dat/{}.dat", bbs, key);
+    return std::format("{}/dat/{}.dat", bbs, key);
 }
-
-using namespace std;
 
 int main()
 {
-	init();
-	testWrite("news4vip", "1234567890", "Hello, 2chDB.");
+    init();
+    testWrite("news4vip", "1234567890", "Hello, 2chDB.");
 
-	cout << "Hello CMake." << endl;
-	cout << queryFromReadCGI("news4vip", "1234567890") << endl;
-	return 0;
+    std::cout << "Hello CMake." << std::endl;
+    std::cout << queryFromReadCGI("news4vip", "1234567890") << std::endl;
+    for (;;) {
+    }
+    return 0;
 }
 
 void init()
 {
-	rawmode = 1;
-	isbusytime = 1;
-	atexit(atexitfunc);
+    rawmode = 1;
+    isbusytime = 1;
+    atexit(atexitfunc);
 }
 
 const char* queryFromReadCGI(const char* bbs, const char* key) {
-	const std::string fname = create_fname(bbs, key);
-	dat_read(fname.c_str(), 0, 0, 0);
+    const std::string fname = create_fname(bbs, key);
+    dat_read(fname.c_str(), 0, 0, 0);
 
-	return BigBuffer;
+    return BigBuffer;
 }
 
 void testWrite(const char* bbs, const char* key, const char* source) {
-	const std::string fname = create_fname(bbs, key);
-	
-	std::ofstream ofs(fname, std::ios_base::out | std::ios_base::binary);
-	if (!ofs) {
-		std::cerr << "Failed to open file: " << fname.data() << std::endl;
-		return;
-	}
+    const std::string fname = create_fname(bbs, key);
+    
+    std::ofstream ofs(fname, std::ios_base::out | std::ios_base::binary);
+    if (!ofs) {
+        std::cerr << "Failed to open file: " << fname.data() << std::endl;
+        return;
+    }
 
-	ofs << source;
+    ofs << source;
 }
